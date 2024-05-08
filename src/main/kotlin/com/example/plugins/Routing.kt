f958824fb011295f1b70f5916fdeced2274f36b7
mikesplore <mikepremium8@gmail.com>
2024-05-08 18:04:35 +0300

    api

--- src/main/kotlin/com/example/plugins/Routing.kt
+++ src/main/kotlin/com/example/plugins/Routing.kt
@@ -1,10 +1,12 @@
 package com.example.plugins
 
+import com.example.routes.customerRouting
 import io.ktor.server.application.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
 
 fun Application.configureRouting() {
     routing {
+        customerRouting()
     }
 }
